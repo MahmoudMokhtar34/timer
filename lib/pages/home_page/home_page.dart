5d9be28810164b89af448220f708ddf675a08eae
Mahmoud Mokhtar <mahmokhtar30@gmail.com>
2023-01-29 10:29:36 +0200

    m

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -545,3 +545,55 @@ class _SettingsState extends State<Settings> {
 //           ),
 //         ],
 //       ),
+
+Widget future() {
+  return FutureBuilder<String>(
+    future: null, // a previously-obtained Future<String> or null
+    builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+      List<Widget> children;
+      if (snapshot.hasData) {
+        children = <Widget>[
+          const Icon(
+            Icons.check_circle_outline,
+            color: Colors.green,
+            size: 60,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16),
+            child: Text('Result: ${snapshot.data}'),
+          ),
+        ];
+      } else if (snapshot.hasError) {
+        children = <Widget>[
+          const Icon(
+            Icons.error_outline,
+            color: Colors.red,
+            size: 60,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16),
+            child: Text('Error: ${snapshot.error}'),
+          ),
+        ];
+      } else {
+        children = const <Widget>[
+          SizedBox(
+            width: 60,
+            height: 60,
+            child: CircularProgressIndicator(),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 16),
+            child: Text('Awaiting result...'),
+          ),
+        ];
+      }
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: children,
+        ),
+      );
+    },
+  );
+}
